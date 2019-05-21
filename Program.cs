6f6e37a5b046851a616f65b3edb143b2a214ed22
Yuki Osaki <yuki.osaki7@gamail.com>
2019-05-21 21:52:18 +0900

    色々

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace lineAralgebra
@@ -7,13 +8,49 @@ namespace lineAralgebra
 	{
 		static void Main(string[] args)
 		{
-			// var N = Console.ReadLine().Split().Select(int.Parse).ToArray()[0];
-			// var H = Console.ReadLine().Split().Select(int.Parse).ToArray();
-			Console.WriteLine("main is called");
+			var inputs = new List<int> { 1, 1, 1, 2, 2, 3, 4, 5, 5 };
 
-			Console.WriteLine("rea");
+			bool ok = Solve(inputs).Sum() == 0;
+
+			if (ok)
+			{
+				Console.WriteLine("Yes");
+			}
+			else
+			{
+				Console.WriteLine("No");
+			}
+
 
 		}
 
+		private static List<int> Solve(List<int> inputs)
+		{
+
+			if (inputs.Sum() == 0 || inputs.Sum() % 2 != 0)
+			{
+				return inputs;
+			}
+
+			for (int i = 0; i < inputs.Count; i++)
+			{
+				if (i >= (inputs.Count - 1 - inputs[inputs.Count - 1]))
+				{
+					inputs[i]--;
+				}
+			}
+
+			inputs.Sort();
+			inputs.RemoveAt(inputs.Count - 1);
+
+			foreach (var item in inputs)
+			{
+				Console.Write(item);
+			}
+			Console.WriteLine("");
+			var deepCopyList = new List<int>(inputs);
+
+			return Solve(inputs);
+		}
 	}
 }

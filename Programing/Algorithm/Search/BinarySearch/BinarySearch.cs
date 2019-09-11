7f0a4dbd30513f9ecc204e1c56773c369cb3148a
yosaki@degica.com <yuki.osaki7@gmail.com>
2019-09-11 14:01:25 +0900

    binaru seachについて考えてみた

--- Programing/Algorithm/Search/BinarySearch/BinarySearch.cs
+++ Programing/Algorithm/Search/BinarySearch/BinarySearch.cs
@@ -49,4 +49,68 @@ public class BinarySearch
 			return Find2(array, target, middle + 1, hi);
 		}
 	}
+
+		// テストコード　fo test
+	     // 配列が偶数ならきれいに分けることができなくて、
+        // 答えが一番端の答えだった時に 要素すうが半分のほうが[1,2 3]でtargetが1の場合
+        //  middle = 2/2 = 1;
+        // そして一番最後にもう一度分割して答えを探していくのでlo = 0 hi = 0ってパターンがあるんだ
+        // これをしなくていいパターンについて再度考えてみたいね
+
+		// static void Main(string[] args)
+		// {
+		// 	Console.WriteLine("Hello World!");
+        //     var input = new int[]{1,3,5,6,7,8};
+
+        //     var ans1 = BinarySearch1(input, 0, input.Length -1, 8);
+        //     Console.WriteLine("serach 1 result" + ans1);
+            
+        //     var ans2 = BinarySearch2(input, 0, input.Length -1, 8);
+        //     Console.WriteLine("serach 2 result" + ans2);
+            
+		// }
+
+
+		// e
+		public static int BinarySearch1(int[] a, int lo, int hi, int target)
+		{
+			while (lo <= hi)
+			{
+				var middle = lo + (hi - lo) / 2;
+                System.Console.WriteLine("lo -> " + lo + " hi -> " + hi + " middle -> " + middle);
+				if (a[middle] == target) return middle;
+				else if (target < a[middle])
+				{
+					hi = middle - 1;
+				}
+				else
+				{
+					lo = middle + 1;
+
+				}
+			}
+
+			return -1;
+		}
+
+		public static int BinarySearch2(int[] a, int lo, int hi, int target)
+		{
+			while (lo < hi)
+			{
+				var middle = lo + (hi - lo) / 2;
+                System.Console.WriteLine("lo -> " + lo + " hi -> " + hi + " middle -> " + middle);
+				if (a[middle] == target) return middle;
+				else if (target < a[middle])
+				{
+					hi = middle - 1;
+				}
+				else
+				{
+					lo = middle + 1;
+
+				}
+			}
+
+            return -1;
+		}
 }

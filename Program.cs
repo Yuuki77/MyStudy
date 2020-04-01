64361a912be8fa043f2572914d7beeadda201d4c
Yuki Osaki <yuki.osaki7@gamail.com>
2020-04-02 00:42:28 +0900

    commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		var a = new PowerOfTwo();
-		a.Solve2(10);
+		var N = long.Parse(Console.ReadLine());
+
+		var ans = 1 << 29;
+		for (long i = 1; i < N / 2; i++)
+		{
+			if (N % i == 0)
+			{
+				long b = N / i;
+				var f = Math.Max(Solve(i), Solve(b));
+				ans = Math.Min(ans, f);
+			}
+		}
+
+		System.Console.WriteLine(ans);
+	}
+
+	static int Solve(int n)
+	{
+		int sum = 0;
+
+		while (n > 0)
+		{
+			sum++;
+			n /= 10;
+		}
+
+		return sum;
 	}
 }
--- Programing/その他/PrimeFactorial.cs
+++ Programing/その他/PrimeFactorial.cs
@@ -1,18 +1,56 @@
+using System;
+using System.Collections.Generic;
 public class PrimeFactorial
 {
-	public void Solve(long n)
+	// 素因数分解
+	public List<Tuple<long, long>> Solve(long n)
 	{
 		System.Console.WriteLine("test");
-
+		var list = new List<Tuple<long, long>>();
 		for (var i = 2; i <= n / 2; i++)
 		{
 			// System.Console.WriteLine(i % n == 0);
 			// System.Console.WriteLine(i);
+			long ex = 0;
 			while (n % i == 0)
 			{
-				System.Console.WriteLine(i);
+				// System.Console.WriteLine(i);
+				ex++;
 				n /= i;
 			}
+
+			list.Add(new Tuple<long, long>(i, ex));
+
 		}
+
+		if (n != 1)
+		{
+			list.Add(new Tuple<long, long>(n, 1));
+		}
+		// ans.Sort();
+
+		return list;
 	}
+
+	// 約数列挙
+	public List<long> Solve2(long n)
+	{
+		var ans = new List<long>();
+		for (var i = 2; i * i <= n; i++)
+		{
+			if (n % i == 0)
+			{
+				ans.Add(i);
+				if (n / i != i)
+					ans.Add(n / i);
+
+			}
+
+		}
+
+		ans.Sort();
+
+		return ans;
+	}
+
 }
